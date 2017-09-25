943aca9d95ec85dfae1985ec6d72aab981163b9f
Devin Alvaro <devin.alvaro@gmail.com>
2017-09-25 21:03:15 +0700

    Rename tab_fragment layouts

--- app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentImages.java
+++ app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentImages.java
@@ -16,6 +16,6 @@ public class TabFragmentImages extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_fragment2, container, false);
+        return inflater.inflate(R.layout.tab_fragment_images, container, false);
     }
 }
--- app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentTexts.java
+++ app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentTexts.java
@@ -16,6 +16,6 @@ public class TabFragmentTexts extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_fragment1, container, false);
+        return inflater.inflate(R.layout.tab_fragment_texts, container, false);
     }
 }
