a846eb93ae71c84b145adede9688b3bdc7f53a82
Devin Alvaro <devin.alvaro@gmail.com>
2017-09-26 22:13:51 +0700

    Implement uploadMemory() method: upload user's file to Firebase

--- app/src/main/java/com/mindworld/howtosurvive/mindworld/MainActivity.java
+++ app/src/main/java/com/mindworld/howtosurvive/mindworld/MainActivity.java
@@ -1,20 +1,68 @@
 package com.mindworld.howtosurvive.mindworld;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 public class MainActivity extends AppCompatActivity {
+    private final int READ_FILE_BROWSER_REQUEST_CODE = 1;
+    private final int READ_EXTERNAL_STORAGE_REQUEST_CODE = 2;
+
+    private StorageReference mStorageRef;
+
+    private void getMemory() {
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        intent.setType("text/plain");
+        intent.setType("image/*");
+        intent.setType("video/*");
+
+        startActivityForResult(intent, READ_FILE_BROWSER_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == READ_FILE_BROWSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                Uri fileUri = data.getData();
+                String mimetype = getContentResolver().getType(fileUri);
+                StorageMetadata metadata = new StorageMetadata.Builder()
+                        .setContentType(mimetype)
+                        .build();
+                StorageReference memoryRef = mStorageRef.child(fileUri.getLastPathSegment());
+                UploadTask uploadTask = memoryRef.putFile(fileUri, metadata);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,6 +96,34 @@ public class MainActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGE_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getMemory();
+                }
+            }
+        }
+    }
+
+    public void uploadMemory(View view) {
+        // check READ_EXTERNAL_STORAGE permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_REQUEST_CODE);
+        } else {
+            getMemory();
+        }
     }
 
     public void writeMemory(View view) {
