aca5482182f0a8e7092ec08ca03848ab94c9a8af
gejimayu <gianffh@gmail.com>
2017-09-29 00:26:42 +0700

    Add feature : Display video

--- app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentVideos.java
+++ app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentVideos.java
@@ -2,11 +2,37 @@ package com.mindworld.howtosurvive.mindworld;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mindworld.howtosurvive.mindworld.models.TextFile;
+import com.mindworld.howtosurvive.mindworld.models.VideoFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TabFragmentVideos extends Fragment {
+    // Main Activity Context
+    View view;
+    // Creating DatabaseReference.
+    DatabaseReference databaseReference;
+
+    // Creating RecyclerView.
+    RecyclerView recyclerView;
+
+    // Creating RecyclerView.Adapter.
+    RecyclerView.Adapter adapter ;
+
+    // Creating List of ImageUploadInfo class.
+    List<VideoFile> list = new ArrayList<>();
 
     public TabFragmentVideos() {
         // Required empty public constructor
@@ -17,6 +43,55 @@ public class TabFragmentVideos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_fragment_videos, container, false);
+        view = inflater.inflate(R.layout.tab_fragment_videos, container, false);
+        // Assign id to RecyclerView.
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview_video);
+
+        // Setting RecyclerView size true.
+        recyclerView.setHasFixedSize(true);
+
+        // Setting RecyclerView layout as LinearLayout.
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        // Setting up Firebase image upload folder path in databaseReference.
+        // The path is already defined in MainActivity.
+        databaseReference = FirebaseDatabase.getInstance().getReference("video");
+
+        // Adding Add Value Event Listener to databaseReference.
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String prevChildKey) {
+
+                VideoFile videoUploadInfo = snapshot.getValue(VideoFile.class);
+
+                list.add(videoUploadInfo);
+
+                adapter = new RecyclerViewAdapterVideo(view.getContext(), list);
+
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        // Inflate the layout for this fragment
+        return view;
     }
 }
