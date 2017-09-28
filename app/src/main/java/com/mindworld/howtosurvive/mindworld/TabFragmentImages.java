c79677e1a0b636004025c6a539a179aa5688690a
gejimayu <gianffh@gmail.com>
2017-09-28 20:48:31 +0700

    Add a feature , retrieving and displaying stored image in fragment

--- app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentImages.java
+++ app/src/main/java/com/mindworld/howtosurvive/mindworld/TabFragmentImages.java
@@ -1,12 +1,38 @@
 package com.mindworld.howtosurvive.mindworld;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mindworld.howtosurvive.mindworld.models.ImageFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TabFragmentImages extends Fragment {
+    // Main Activity Context
+    View view;
+    // Creating DatabaseReference.
+    DatabaseReference databaseReference;
+
+    // Creating RecyclerView.
+    RecyclerView recyclerView;
+
+    // Creating RecyclerView.Adapter.
+    RecyclerView.Adapter adapter ;
+
+    // Creating List of ImageUploadInfo class.
+    List<ImageFile> list = new ArrayList<>();
 
     public TabFragmentImages() {
         // Required empty public constructor
@@ -15,7 +41,43 @@ public class TabFragmentImages extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view =  inflater.inflate(R.layout.tab_fragment_images, container, false);
+        // Assign id to RecyclerView.
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview_image);
+
+        // Setting RecyclerView size true.
+        recyclerView.setHasFixedSize(true);
+
+        // Setting RecyclerView layout as LinearLayout.
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        // Setting up Firebase image upload folder path in databaseReference.
+        // The path is already defined in MainActivity.
+        databaseReference = FirebaseDatabase.getInstance().getReference("image");
+
+        // Adding Add Value Event Listener to databaseReference.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+                    ImageFile imageUploadInfo = postSnapshot.getValue(ImageFile.class);
+
+                    list.add(imageUploadInfo);
+                }
+
+                adapter = new RecyclerViewAdapterImage(view.getContext(), list);
+
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_fragment_images, container, false);
+        return view;
     }
 }
