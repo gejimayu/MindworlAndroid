dcda4132aa53d9974ca00ec69c0396095c8ebc80
Devin Alvaro <devin.alvaro@gmail.com>
2017-10-01 00:06:31 +0700

    Refactor codes: Season 2 (Add MindMemory and UserCity class)

--- app/src/main/java/com/mindworld/howtosurvive/mindworld/MainActivity.java
+++ app/src/main/java/com/mindworld/howtosurvive/mindworld/MainActivity.java
@@ -10,13 +10,6 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Address;
-import android.location.Criteria;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
@@ -31,37 +24,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.mindworld.howtosurvive.mindworld.models.ImageFile;
-import com.mindworld.howtosurvive.mindworld.models.TextFile;
-import com.mindworld.howtosurvive.mindworld.models.VideoFile;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     public static final String EXTRA_REPLY = "com.mindworld.howtosurvive.mindworld.extra.REPLY";
     public static final String EXTRA_UID = "com.mindworld.howtosurvive.mindworld.extra.UID";
 
-    private static final int ACCESS_COARSE_LOCATION_REQUEST_CODE = 2003;
+    private static final int ACCESS_COARSE_LOCATION_PERMISSION_CODE = 2003;
     private static final int READ_FILE_BROWSER_REQUEST_CODE = 2001;
-    private static final int READ_EXTERNAL_STORAGE_REQUEST_CODE = 2002;
-
-    private static final String FIRST_TIMER = "FIRST_TIMER";
+    private static final int READ_EXTERNAL_STORAGE_PERMISSION_CODE = 2002;
 
     private static final String SHARED_PREF_FILE = "com.mindworld.howtosurvive.mindworld";
-
-    String filename;
-    String filelocation;
-    String mimetype;
 
     Sensor mAccelerometer;
     Sensor mMagnetometer;
@@ -78,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private DatabaseReference mDatabaseRef;
     private SharedPreferences mPreferences;
     private SensorManager mSensorManager;
+
+    private UserCity mUserCity;
+    private MindMemory mMindMemory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,16 +106,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mPreferences = getSharedPreferences(SHARED_PREF_FILE, MODE_PRIVATE);
         final SharedPreferences.Editor preferencesEditor = mPreferences.edit();
         // check if the user use the app for the first time
-        if (mPreferences.getBoolean(FIRST_TIMER, true)) {
+        if (mPreferences.getBoolean("FIRST_TIMER", true)) {
             Toast.makeText(getApplicationContext(), "Welcome to Mindworld.", Toast.LENGTH_LONG).show();
 
-            preferencesEditor.putBoolean(FIRST_TIMER, false).apply();
+            preferencesEditor.putBoolean("FIRST_TIMER", false).apply();
         }
 
         // initialize device sensors
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        // initialize UserCity
+        mUserCity = new UserCity(this);
+        findUserCity();
+
+        // initialize MindMemory
+        mMindMemory = new MindMemory(this, mUserId, mStorageRef, mDatabaseRef, mUserCity.getFilelocation());
     }
 
     @Override
@@ -179,18 +165,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         switch (requestCode) {
-            case READ_EXTERNAL_STORAGE_REQUEST_CODE: {
+            case READ_EXTERNAL_STORAGE_PERMISSION_CODE: {
                 // permission to read external storage granted
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getMemory();
+                    startActivityForResult(mMindMemory.getMemory(), READ_FILE_BROWSER_REQUEST_CODE);
                 }
             }
-            case ACCESS_COARSE_LOCATION_REQUEST_CODE: {
+            case ACCESS_COARSE_LOCATION_PERMISSION_CODE: {
                 // permission to read external storage granted
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getCity();
+                    mUserCity.getCity();
                 }
             }
         }
@@ -202,58 +188,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         if (requestCode == READ_FILE_BROWSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             if (data != null) {
-                // get selected file's URI
-                Uri fileUri = data.getData();
-                // build file's metadata
-                mimetype = getContentResolver().getType(fileUri);
-                StorageMetadata metadata = new StorageMetadata.Builder()
-                        .setContentType(mimetype)
-                        .build();
-                // upload file to Firebase Cloud Storage
-                StorageReference memoryRef = mStorageRef.child("user/" + mUserId + "/" + fileUri.getLastPathSegment());
-                UploadTask uploadTask = memoryRef.putFile(fileUri, metadata);
-
-                // get file name
-                filename = fileUri.getLastPathSegment();
-
-                // get file location
-                accessCity();
-
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        Toast.makeText(getApplicationContext(), "Upload failed ", Toast.LENGTH_LONG).show();
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
-                        @SuppressWarnings("VisibleForTests")
-                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                        // Showing toast message after done uploading.
-                        Toast.makeText(getApplicationContext(), "File Uploaded Successfully ", Toast.LENGTH_LONG).show();
-
-                        DatabaseReference db;
-                        if (mimetype.contains("image")) {
-                            @SuppressWarnings("VisibleForTests")
-                            ImageFile imageUploadInfo = new ImageFile(filename, filelocation,
-                                    taskSnapshot.getDownloadUrl().toString());
-                            //push into database
-                            db = mDatabaseRef.child("image").push();
-                            db.setValue(imageUploadInfo);
-                        } else if (mimetype.contains("text")) {
-                            TextFile txt = new TextFile(filename, filelocation);
-                            // push into database
-                            db = mDatabaseRef.child("text").push();
-                            db.setValue(txt);
-                        } else if (mimetype.contains("video")) {
-                            VideoFile txt = new VideoFile(filename, filelocation);
-                            // push into database
-                            db = mDatabaseRef.child("video").push();
-                            db.setValue(txt);
-                        }
-                    }
-                });
+                mMindMemory.uploadMemory(data);
             }
         }
     }
@@ -302,96 +237,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void uploadMemory(View view) {
         // check permission to read external storage
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_REQUEST_CODE);
+                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_PERMISSION_CODE);
         } else {
-            getMemory();
+            startActivityForResult(mMindMemory.getMemory(), READ_FILE_BROWSER_REQUEST_CODE);
         }
     }
 
-    private void getMemory() {
-        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
-        // browser.
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-
-        // Filter to only show results that can be "opened", such as a
-        // file (as opposed to a list of contacts or timezones)
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        // Filter to show only texts, images, and videos using their MIME data types.
-        String[] mimeTypes = {"text/plain", "image/*", "video/*"};
-        intent.setType("*/*");
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-
-        startActivityForResult(intent, READ_FILE_BROWSER_REQUEST_CODE);
-    }
-
-    private void accessCity() {
+    public void findUserCity() {
         // check permission to access user location
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, ACCESS_COARSE_LOCATION_REQUEST_CODE);
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, ACCESS_COARSE_LOCATION_PERMISSION_CODE);
         } else {
-            getCity();
-        }
-    }
-
-    private void getCity() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Getting LocationManager object from System Service LOCATION_SERVICE
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-            // Creating a criteria object to retrieve provider
-            Criteria criteria = new Criteria();
-
-            // Getting the name of the best provider
-            String provider = locationManager.getBestProvider(criteria, true);
-
-            // Getting Current Location From GPS
-            Location location = locationManager.getLastKnownLocation(provider);
-
-            if (location != null) {
-                getUserGeoInfo(location.getLatitude(), location.getLongitude());
-            }
-
-            // Define a listener that responds to location updates
-            LocationListener locationListener = new LocationListener() {
-                public void onLocationChanged(Location location) {
-                    // Called when a new location is found by the network location provider.
-                    getUserGeoInfo(location.getLatitude(), location.getLongitude());
-                }
-
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-                }
-
-                public void onProviderEnabled(String provider) {
-                }
-
-                public void onProviderDisabled(String provider) {
-                }
-            };
-
-            // Set how often you want to request location updates and where you want to receive them
-            locationManager.requestLocationUpdates(provider, 20000, 0, locationListener);
-        }
-    }
-
-    private void getUserGeoInfo(double latitude, double longitude) {
-        Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
-        if (Geocoder.isPresent()) {
-            try {
-                List<Address> addresses = geoCoder.getFromLocation(latitude, longitude, 1);
-
-                if (addresses.size() > 0) {
-                    // obtain all information from addresses.get(0)
-                    filelocation = addresses.get(0).getLocality();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            mUserCity.getCity();
         }
     }
 }
---
+++ app/src/main/java/com/mindworld/howtosurvive/mindworld/MindMemory.java
@@ -0,0 +1,100 @@
+package com.mindworld.howtosurvive.mindworld;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.mindworld.howtosurvive.mindworld.models.ImageFile;
+import com.mindworld.howtosurvive.mindworld.models.TextFile;
+import com.mindworld.howtosurvive.mindworld.models.VideoFile;
+
+public class MindMemory {
+    private Context mContext;
+    private String mUserId;
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private String mFilelocation;
+
+    public MindMemory(Context context, String userId, StorageReference storageRef, DatabaseReference databaseRef, String filelocation) {
+        mContext = context;
+        mUserId = userId;
+        mStorageRef = storageRef;
+        mDatabaseRef = databaseRef;
+        mFilelocation = filelocation;
+    }
+
+
+    public Intent getMemory() {
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only texts, images, and videos using their MIME data types.
+        String[] mimeTypes = {"text/plain", "image/*", "video/*"};
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+
+        return intent;
+    }
+
+    public void uploadMemory(Intent data) {
+        // get selected file's URI
+        Uri fileUri = data.getData();
+        // build file's metadata
+        final String mimetype = mContext.getContentResolver().getType(fileUri);
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType(mimetype)
+                .build();
+        // upload file to Firebase Cloud Storage
+        StorageReference memoryRef = mStorageRef.child("user/" + mUserId + "/" + fileUri.getLastPathSegment());
+        UploadTask uploadTask = memoryRef.putFile(fileUri, metadata);
+
+        // get file name
+        final String filename = fileUri.getLastPathSegment();
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(mContext.getApplicationContext(), "Upload failed.", Toast.LENGTH_LONG).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Showing toast message after done uploading.
+                Toast.makeText(mContext.getApplicationContext(), "Upload succeed.", Toast.LENGTH_LONG).show();
+
+                DatabaseReference databaseReference;
+                if (mimetype.contains("image")) {
+                    @SuppressWarnings("VisibleForTests")
+                    ImageFile imageUploadInfo = new ImageFile(filename, mFilelocation,
+                            taskSnapshot.getDownloadUrl().toString());
+                    //push into database
+                    databaseReference = mDatabaseRef.child("image").push();
+                    databaseReference.setValue(imageUploadInfo);
+                } else if (mimetype.contains("text")) {
+                    TextFile txt = new TextFile(filename, mFilelocation);
+                    // push into database
+                    databaseReference = mDatabaseRef.child("text").push();
+                    databaseReference.setValue(txt);
+                } else if (mimetype.contains("video")) {
+                    VideoFile txt = new VideoFile(filename, mFilelocation);
+                    // push into database
+                    databaseReference = mDatabaseRef.child("video").push();
+                    databaseReference.setValue(txt);
+                }
+            }
+        });
+    }
+}
